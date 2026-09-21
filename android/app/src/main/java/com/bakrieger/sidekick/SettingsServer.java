84c5d4233b6f1d46b82730a6db6fa547ab7cd09e
bakrieger428 <bakrieger428@users.noreply.github.com>
2026-09-21 16:20:11 -0400

    Add missing File import in SettingsServer

--- android/app/src/main/java/com/bakrieger/sidekick/SettingsServer.java
+++ android/app/src/main/java/com/bakrieger/sidekick/SettingsServer.java
@@ -3,6 +3,7 @@ package com.bakrieger.sidekick;
 import android.content.Context;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
